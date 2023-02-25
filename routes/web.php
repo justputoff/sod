055692098c32101fb07f9fc63dd02570d80261a8
justputoff <kholid.hasibuan35@gmail.com>
2023-02-25 15:51:54 +0700

    Home

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return "Halaman Home";
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
